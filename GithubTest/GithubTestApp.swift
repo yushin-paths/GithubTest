6858a3566a981b61d3449e0afd0b63aded9b9138
Yushin <yushin@hupapp.com>
2022-02-15 12:13:03 +1300

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Yushin Kim on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
